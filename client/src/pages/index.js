3b9668657ac71330e1c772464fdeeb14fc2d7a11
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2023-07-06 21:17:11 +0100

    update

--- client/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as About } from "./About/About";
-export { default as Careers } from "./careers/Careers";
-export { default as Support } from "./support/Support";
-export { default as LandingPage } from "./landing/LandingPage";
-export { default as NoPage } from "./404/NoPage";
-export { default as ProductDetails } from "./products/ProductDetails";
-export { default as Products } from "./products/Products";
